7bea79b134cb6155e62cb730f6eb4917270beb43
Eric Oliveira <ericjmoliveira@gmail.com>
2022-09-16 15:58:05 -0300

    Create global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import QuizProvider from './providers/QuizProvider';
+import GlobalStyles from './styles/GlobalStyles';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <QuizProvider>
+            <GlobalStyles />
             <App />
         </QuizProvider>
     </React.StrictMode>
--- src/providers/QuizProvider.tsx
+++ src/providers/QuizProvider.tsx
@@ -3,7 +3,7 @@ import { useReducer } from 'react';
 import QuizContext from '../contexts/QuizContext';
 import { quizReducer, initialState } from '../reducers/quizReducer';
 
-export default function QuizProvider({ children }: { children: JSX.Element }) {
+export default function QuizProvider({ children }: { children: JSX.Element[] }) {
     const [state, dispatch] = useReducer(quizReducer, initialState);
 
     return <QuizContext.Provider value={{ state, dispatch }}>{children}</QuizContext.Provider>;
